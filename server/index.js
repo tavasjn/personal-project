ac4b024be572345a154d9a61456cfb20c9e760ea
Tavas Neuwirth <tavasjn_24@hotmail.com>
2019-10-29 15:03:01 -0600

    initial set up

---
+++ server/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+// const mainCtrl = require('./controller/mainController');
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+const app = express();
+
+app.use(express.json());
+
+// Connect to the data base //
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('DB Connected')
+});
+
+// Cookie Session Below //
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}));
+
+// Main CTRL EndPoints Below //
+
+
+
+
+const port = SERVER_PORT;
+app.listen(port, () => console.log(`Magic on Port: ${port}`));
