117cc743551b3325af8a23d3e8c63bf8fb0ea32a
Tavas Neuwirth <tavasjn_24@hotmail.com>
2019-10-30 16:54:26 -0600

    middle of header

--- src/Components/Components/Header/Header.js
+++ src/Components/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import './Header.css';
+
 
 class Header extends Component {
     render(){
         return(
-            <div>
-                Header
+            <div className='nav-bar'>
+                <div className='nav-buttons'>Title</div>
+                <div className='nav-buttons'>My Account</div>
+                <div className='nav-buttons'>Find a Dog</div>
+                <div className='nav-buttons'>Explore All Dogs</div>
             </div>
         )
     }
