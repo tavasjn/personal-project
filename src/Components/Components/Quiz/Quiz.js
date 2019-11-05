f34ad7ecaaa28b02e84ad6801a1a4a4cc3fbdb13
Tavas  Neuwirth <tavasneuwirth@Tavass-MacBook-Pro.local>
2019-11-05 15:39:21 -0700

    working on styling as well as quiz

--- src/Components/Components/Quiz/Quiz.js
+++ src/Components/Components/Quiz/Quiz.js
@@ -1,12 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+// This page is the layout for the quiz and will house all the questions //
+// Depending on the sequence they choose the questions
+// we will then project the top three choices for them using tensorFlow
+
+
+// Style of Page below //
+import './Quiz.css';
 import Header from '../Header/Header';
 
+
+
 class FindDog extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Header />
-                FindDog
+                <div className='quiz-body-page'>
+                    <div className='question-box'>
+                        <div className='question'>A Dog that is good for hunting?</div>
+                        <div className='quiz-page-spreading'>
+                            <button className='quiz-buttons'>Yes!</button>
+                            <button className='quiz-buttons'>No!</button>
+                        </div>
+                    </div>
+                    <div className='question-box'>
+                        <div className='question'>A Dog that is good for hunting?</div>
+                        <div className='quiz-page-spreading'>
+                            <button className='quiz-buttons'>Yes!</button>
+                            <button className='quiz-buttons'>No!</button>
+                        </div>
+                    </div>
+                    <div className='question-box'>
+                        <div className='question'>Soft fur?</div>
+                        <div className='quiz-page-spreading'>
+                            <button className='quiz-buttons'>Yes!</button>
+                            <button className='quiz-buttons'>No!</button>
+                        </div>
+                    </div>
+                    <div className='question-box'>
+                        <div className='question'>Hyoallergenic?</div>
+                        <div className='quiz-page-spreading'>
+                            <button className='quiz-buttons'>Yes!</button>
+                            <button className='quiz-buttons'>No!</button>
+                        </div>
+                    </div>
+                    <div className='question-box-size'>
+                        <div className='question-size'>What size dog are you looking for?</div>
+                        <div className='quiz-page-spreading-size'>
+                            <button className='quiz-buttons-size'>Small</button>
+                            <button className='quiz-buttons-size'>Small-Medium</button>
+                            <button className='quiz-buttons-size'>Medium</button>
+                            <button className='quiz-buttons-size'>Medium-Large</button>
+                            <button className='quiz-buttons-size'>Large</button>
+                        </div>
+                    </div>
+                    <div>
+                        <button className='finish-quiz-button'>Complete</button>
+                    </div>
+                </div>
             </div>
         )
     }
