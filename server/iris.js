ebfdd6b0e50fefdb91d8745fa85745efd3fe1153
Tavas  Neuwirth <tavasneuwirth@Tavass-MacBook-Pro.local>
2019-11-07 16:09:33 -0700

    adding my tensorflow

---
+++ server/iris.js
@@ -0,0 +1,12 @@
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+
+const iris = require('../iris.json');
+
+module.exports = {
+    async runModel(req, res){
+        let{
+            
+        }
+    }
+}
